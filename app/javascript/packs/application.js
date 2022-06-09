25a5f74da0d25b2c7903a7b3922555c47b128d3f
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-09 16:28:16 +0900

    Add: application.scssを読み込む記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap'
+import '../stylesheets/application.scss'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
