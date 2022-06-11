b87501246ddc25a3c03b63864bd57d1919de3b31
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-11 14:06:46 +0900

    Add: erbにJavascriptのヘルパー（<%= image_pack_tag %>）が利用できる様に定義

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,69 +8,18 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import 'bootstrap'
-import '../stylesheets/application.scss'
-
+// 上で読み込んだものを起動
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-/*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+import 'bootstrap'
+import '../stylesheets/application.scss'
 
-window.addEventListener('DOMContentLoaded', event => {
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
 
-  // Navbar shrink function
-  var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-          return;
-      }
-      if (window.scrollY === 0) {
-          navbarCollapsible.classList.remove('navbar-shrink')
-      } else {
-          navbarCollapsible.classList.add('navbar-shrink')
-      }
-
-  };
-
-  // Shrink the navbar 
-  navbarShrink();
-
-  // Shrink the navbar when page is scrolled
-  document.addEventListener('scroll', navbarShrink);
-
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          offset: 74,
-      });
-  };
-
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
-  const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
-  });
-
-  // Activate SimpleLightbox plugin for portfolio items
-  new SimpleLightbox({
-      elements: '#portfolio a.portfolio-box'
-  });
-
-});
+// Webpackerに画像ファイルをインポート
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
