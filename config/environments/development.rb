d7a75a199621cd0dc42fcf38fee25bb633a2e1d9
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-14 19:09:31 +0900

    Update: GメールアドレスとGメールパスワードの部分を自分のアカウントのものに変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,18 +42,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  
+  config.action_mailer.raise_delivery_errors = true
 
-  
+  config.action_mailer.delivery_method = :smtp
 
-  
-    
-    
-    
- 
-  
-  
-  
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'sasakix69@gmail.com',
+    :password => 'smpdaeiffgclgniu',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
