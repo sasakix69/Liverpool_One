3e6af9d276aafd366f32cfd3d4a81a99eeb19b8e
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-14 18:37:19 +0900

    Update: root_urlとユーザーログイン後のshowページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'tops#index'
+  get 'tops/show'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
