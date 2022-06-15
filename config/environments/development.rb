5eb9bc5334f439deb9ae3c213ac8a9c57591ffcf
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-15 10:54:07 +0900

    Fix: メール送信用のaction mailerをdatenvを使って設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,20 +40,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :user_name => 'sasakix69@gmail.com',
-    :password => 'smpdaeiffgclgniu',
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['EMAIL_ADDRESS'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
