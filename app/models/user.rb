b27aa3ab6c389f28c981daa226c0465dc8768893
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-15 18:42:50 +0900

    Fix: メールアドレスでの認証も実装している為、OAuthでの認証時もメールアドレスを保存する必要があることから、self.dummy_emailでダミーemailを生成するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers: %i[twitter]
+         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
 
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      user.username = auth['info']['nickname']
-    end
-  end
-
-  def self.new_with_session(params, session)
-    if session['devise.user_attributes']
-      new(session['devise.user_attributes']) do |user|
-        user.attributes = params
-      end
-    else
-      super
-    end
-  end
+        def self.find_for_oauth(auth)
+          user = User.where(uid: auth.uid, provider: auth.provider).first
+      
+          unless user
+            user = User.create(
+              uid:      auth.uid,
+              provider: auth.provider,
+              username: auth.info.nickname,
+              email:    User.dummy_email(auth),
+              password: Devise.friendly_token[0, 20]
+            )
+          end
+          # ダミーメールに承認メールが送られてしまうので、送られないように設定
+          user.skip_confirmation!
+          user
+        end
+      
+        private
+      
+        def self.dummy_email(auth)
+          "#{auth.uid}-#{auth.provider}@example.com"
+        end
 end
