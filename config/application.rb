a077fb66fba53917c9ad9030f42f89cb62f61102
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-16 17:07:24 +0900

    Add: タイムゾーンを設定

--- config/application.rb
+++ config/application.rb
@@ -41,10 +41,13 @@ module LiverpoolOne
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Asia/Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    
+   
   end
 end
