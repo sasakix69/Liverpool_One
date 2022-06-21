b94b536755af3938e2ceb8d3559602043a9ff47a
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-21 15:56:14 +0900

    Update: Twitterでのログイン後遷移先を指定

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,4 +18,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def after_sign_in_path_for(_resource)
+    tops_show_path
+  end
+  
 end
