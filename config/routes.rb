bd63f519c8ee3b445a453eaa403f18f97b5e13c7
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-21 14:26:41 +0900

    Update: Deviseのリダイレクト先の変更をする為、ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   root 'tops#index'
   get 'tops/show'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
