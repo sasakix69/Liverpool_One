b933b02554c3eec205825e0cb135b76e74b40a58
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-22 17:01:57 +0900

    Update: Twitterログインの際に、Twitterのアイコンをプロフィール画像としてそのまま使えるように追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,9 @@ class User < ApplicationRecord
       provider: auth.provider,
       username: auth.info.nickname,
       email: User.dummy_email(auth),
-      password: Devise.friendly_token[0, 20]
+      password: Devise.friendly_token[0, 20],
+      # carrierwaveのアップローダーを:imageにマウントさせてるのでクロスドメインで情報を取得する場合、image:からremote_image_urlに変更
+      remote_image_url: auth.info.image
     )
     # ダミーメールに承認メールが送られてしまうので、送られないように設定
     user.skip_confirmation!
