33f8da0a2d93250dd1ec31715e88f20d87083f18
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-24 10:57:41 +0900

    Fix: Carrierwaveの設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,13 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION'],
+    }
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'arn:aws:s3:::oneliverpool'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
-
+    config.fog_directory  = 'oneliverpool'
+    config.cache_storage = :fog
+  end
 end
