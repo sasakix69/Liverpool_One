e2f90513bcf903a8918bd81fabbc175d41ae0a89
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-24 14:41:51 +0900

    Update: ユーザー、ツイート、コメントのアソシエーションを設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :tweet
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
 
   validates :username, presence: true
+  has_many :tweets
+  has_many :comments
+
   mount_uploader :image, ImageUploader
 
   def self.find_for_oauth(auth)
