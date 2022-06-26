eab42fa89d024ea39bf89dbaff0a524682e20152
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-26 14:26:25 +0900

    Add: フラッシュメッセージの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :success, :info, :warning, :danger
 
   private
 
