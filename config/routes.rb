dacf2dbab277f4f7bf0a4c6aa1e8e240dc413f25
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-26 09:47:06 +0900

    Update: ツイートに関してのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: [:show]
+  resources :tweets
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
