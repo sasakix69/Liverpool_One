20c8fb067c300a76ce8672d4c9fbab95d07bf111
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-26 09:44:08 +0900

    Update: ツイートの本文には入力必須のバリデーションを設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :body, presence: true, length: { maximum: 140 }
+  mount_uploader :tweet_image, TweetImageUploader
 end
