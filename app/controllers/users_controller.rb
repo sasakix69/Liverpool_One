b1b55afdb8909f7e971af199982e8c392dd4c960
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-06-29 11:09:11 +0900

    Update: いいねした投稿を探し、@bookmark_tweetsに格納

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:bookmarks]
+
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order(updated_at: :desc)
+  end
+
+  def bookmarks
+    bookmarks = Bookmark.where(user_id: @user.id).pluck(:tweet_id)
+    @bookmark_tweets = Tweet.find(bookmarks)
+  end  
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
