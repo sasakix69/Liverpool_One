dc0315fc527dac1423778ed1c95b6640c8947e49
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-06 17:08:27 +0900

    Update: ツイートのデフォルト画像を変更

--- app/uploaders/tweet_image_uploader.rb
+++ app/uploaders/tweet_image_uploader.rb
@@ -24,7 +24,7 @@ class TweetImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    'default.jpg'
+    'default_tweet_image.jpg'
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
