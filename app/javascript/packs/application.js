5c40083ee7af94943c60e55aff65b33be44ae4fa
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-06 15:39:21 +0900

    Update: 画像をプレビューするjsファイルを読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,8 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+import '../src/preview/tweet_image_preview'
+import '../src/preview/avatar_preview'
 import '../src/close_flash'
 import '../src/football/schedule'
 import '../src/football/ranking'
