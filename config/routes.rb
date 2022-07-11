e61bc36c0e37e05a45c7631b686507331ede0262
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-11 13:21:58 +0900

    Add: `rails g rails_admin:install`の実行で、管理者画面へのルーティングとrails_adminの設定ファイルの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'tops#index'
   get 'tops/show'
   get 'football/ranking'
