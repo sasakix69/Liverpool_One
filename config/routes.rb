edbb574493f8689f8ecd79855f54b1db6f7f5793
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-11 14:02:48 +0900

    Refactor: 余分なコメントアウトを削除

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,4 @@ Rails.application.routes.draw do
   resources :bookmarks, only: %i[create destroy]
 
   resources :news, only: :index
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
