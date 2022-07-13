abe86e6edf9521f645492075cb107d4bc65ec010
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-13 18:53:26 +0900

    Update: ニュース取得のキーワードを「liverpool-fc」に変更

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
   def index
     require 'news-api'
     news = News.new(ENV['NEWS_API_KEY'])
-    @news = news.get_everything(q: 'liverpool-fc OR football',
+    @news = news.get_everything(q: 'liverpool-fc',
                                 language: 'jp',
                                 sortBy: 'publishedAt')
   end
