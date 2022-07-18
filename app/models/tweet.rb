05f9c9271e95f92331e6b0d4d1dd062d19b30216
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-18 19:41:44 +0900

    Update: いいね通知の作成メソッドを定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,22 @@ class Tweet < ApplicationRecord
 
   validates :body, presence: true, length: { maximum: 140 }
   mount_uploader :tweet_image, TweetImageUploader
+
+  def create_notification_bookmark!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and tweet_id = ? and action = ? ", current_user.id, user_id, id, 'bookmark'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        tweet_id: id,
+        visited_id: user_id,
+        action: 'bookmark'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
