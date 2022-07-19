8cbc8169b18ca83d93261bb16766251ee65d7eb3
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-20 02:29:20 +0900

    Update: UserモデルにRelationshipモデルとのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ApplicationRecord
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   # passive_notifications：相手からの通知
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+  has_many :relationships
+  # フォローしているuserを取得
+  has_many :followings, through: :relationships, source: :follow
+  # followingsテーブルからフォローしている人のデータを取得
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  # userテーブルから自分をフォローしているuserを取得
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   mount_uploader :avatar, AvatarUploader
 
