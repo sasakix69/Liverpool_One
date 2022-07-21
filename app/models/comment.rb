d62f8aa0489ed2b43c5173daf0aa8a388d89738f
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-07-21 23:02:50 +0900

    Refactor: Rubocopによるコード修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
   has_many :notifications, dependent: :destroy
-  
+
   validates :body, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -78,24 +78,22 @@ class User < ApplicationRecord
 
   # 既にフォローされている場合にフォローが重複して保存されることを防ぐ
   def follow(other_user)
-    unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
-    end
+    relationships.find_or_create_by(follow_id: other_user.id) unless self == other_user
   end
 
   # フォローしていればフォローできないようにする
   def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship = relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
 
   # フォローしている user を取得し、include?(other_user)によって自分自身が含まれていないかを確認
   def following?(other_user)
-    self.followings.include?(other_user)
+    followings.include?(other_user)
   end
 
   def create_notification_follow!(current_user)
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    temp = Notification.where(['visitor_id = ? and visited_id = ? and action = ? ', current_user.id, id, 'follow'])
     if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: id,
