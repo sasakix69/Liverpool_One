932936dc9f59ee322829637575b095a51cea1e4b
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-09-08 15:42:40 +0900

    Add: ユーザーのFactoryBotを作成

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    username              {"test"}
+    email                 { "TEST@example.com" }
+    password              {"testuser"}
+    password_confirmation {"testuser"}
+    after(:create) { |user| user.confirm }
+  end
+end
