c0a0494c9efa27a1409603a2085728f2d546624e
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-09-09 18:51:17 +0900

    Add: フォロー機能に関するFactoryBotを作成

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :relationship do
+    association :user
+    follow_id    { FactoryBot.create(:user).id }
+    user_id      { FactoryBot.create(:user).id }
   end
 end
