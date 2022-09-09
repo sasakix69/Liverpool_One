049aa40b084c0f98562793c7b46e16fb48943791
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-09-09 10:56:12 +0900

    Remove: 余分なメソッドを削除

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,13 +1,3 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
-
-module SignInSupport
-  def sign_in(user)
-    visit user_session_path
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    click_button 'ログイン'
-    expect(current_path).to eq(tweets_path)
-  end
-end
