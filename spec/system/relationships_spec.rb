820b390defcc30637f90e1aa47a20cd1312370e3
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-09-09 18:52:46 +0900

    Add: フォロー機能に関するシステムスペックを作成

---
+++ spec/system/relationships_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Relationships', type: :system do
+  before do
+    @user1 = create(:user)
+    @user2 = create(:user)
+  end
+
+  describe '#create,#destroy' do
+    it 'ユーザーをフォロー、フォロー解除できる' do
+      # @user1としてログイン
+      sign_in(@user1)
+
+      # @user1として@user2詳細ページへ遷移する
+      visit user_path(@user2.id)
+
+      # @user2をフォローする
+      expect(page).to have_button('フォローする')
+      click_on('フォローする')
+      expect(page).to have_button('フォローを解除する')
+      expect(@user2.followers.count).to eq(1)
+      expect(@user1.followings.count).to eq(1)
+
+      # @user2をフォロー解除する
+      expect(page).to have_button('フォローを解除する')
+      click_on('フォローを解除する')
+      expect(page).to have_button('フォローする')
+      expect(@user2.followers.count).to eq(0)
+      expect(@user1.followings.count).to eq(0)
+    end
+  end
+end
