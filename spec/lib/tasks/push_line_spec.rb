7a37957c8f37b73a23e64491003c64fcf48f3ed6
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-09-30 14:57:55 +0900

    Add: RSpecにてRakeタスクのテストコードを記述

---
+++ spec/lib/tasks/push_line_spec.rb
@@ -0,0 +1,9 @@
+require 'rake_helper'
+
+describe 'push_line:push_line_message' do
+  subject(:task) { Rake.application['push_line:push_line_message'] }
+
+  it '試合時間10分前にラインメッセージが送信されること' do
+    expect { task.invoke }.not_to raise_error
+  end 
+end
