5f500a4704da19da1423bcabfe2f88d76d7e2d8e
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2023-04-08 18:00:47 +0900

    fix: 本番環境のマイグレーションを実行

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+rails db:migrate
 
 rm -f tmp/pids/server.pid && bin/rails s
