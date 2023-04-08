718cb74b69758c1c642485ba3abdc2520e4ad9a5
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2023-04-08 17:51:41 +0900

    fix: 本番環境のダミーデータを削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-bin/rails db:migrate
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 
 rm -f tmp/pids/server.pid && bin/rails s
